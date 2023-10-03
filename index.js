764ff70563c85fd75c97cb41ace04978c7d34ebe
victor <victor.njutha@student.moringaschool.com>
2023-10-02 22:38:50 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//MULTIPLYING
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
 
-// Write your code here
+//RANDOM INTEGER 
+var random = Math.random () * 10;
+
+//REMEINDER 
+var num3 = 20;
+var num4 = 8;
+var mod = (num3 % num4+ num4) %num4;
+
+//MAX
+var numbers = [5,10,15,20,8];
+var max = Math.max(...numbers);
+if (max !== 20) {
+    max=20;
+}
+
+
+
+
